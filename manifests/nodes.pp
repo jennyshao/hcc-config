11daf2f08ade009f07864e6caef66c626e74ae40
Garhan Attebury <attebury@cse.unl.edu>
2010-09-28 16:47:29 -0500

    Another test

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -4,9 +4,13 @@ node basenode {
 	include ntp
 }
 
+node red-worker inherits basenode {
+	include condor
+}
+
 node 'node001' inherits basenode {
 	include condor
 }
 
-node 'node002' inherits basenode {
+node 'node002' inherits red-worker {
 }
