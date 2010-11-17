1d3d739bf909093948210ec19e58429a646bd0da
Garhan Attebury <attebury@cse.unl.edu>
2010-11-17 14:21:49 -0600

    initial fetch-crl module addition

---
+++ modules/fetch-crl/manifests/init.pp
@@ -0,0 +1,42 @@
+# Class: fetch-crl
+#
+# This class maintains /etc/fetch-crl.conf
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+
+
+class fetch-crl {
+
+	$squidServer = ['red-squid1.unl.edu']
+
+	package { fetch-crl: ensure => latest }
+
+	file { "/etc/fetch-crl.conf":
+		owner  => "root",
+		group  => "root",
+		mode   => 644,
+		content => template("fetch-crl/fetch-crl.conf.erb"),
+		ensure => present,
+		require => Package["fetch-crl"],
+	}
+
+	service { "fetch-crl-boot":
+		enable => true,
+		ensure => running,
+		requirer => Package["fetch-crl"],
+	}
+
+	service { "fetch-crl-cron":
+		enable => true,
+		ensure => running,
+		require => Package["fetch-crl"],
+	}
+
+} # class fetch-crl
