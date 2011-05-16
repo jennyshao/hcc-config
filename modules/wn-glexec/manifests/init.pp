e6a69abb00b0b3692a84d915f408e3ae3af617b1
root <root@red-man.unl.edu>
2011-05-16 15:23:23 -0500

    glexec

--- modules/wn-glexec/manifests/init.pp
+++ modules/wn-glexec/manifests/init.pp
@@ -8,13 +8,88 @@
 class wn-glexec {
    
    # build the directory Structure
-    
+   file { "/etc/glexec":
+		ensure => "directory",
+      owner => "root", group => "root", mode => 644,
+   }
+   file { "/etc/glexec/contrib" :
+      ensure => "directory",
+      owner => "root", group => "root", mode => 644,
+   }
+
+   file { "/etc/glexec/lcas" :
+      ensure => "directory",
+      owner => "root", group => "root", mode => 644,
+   }
+
+   file { "/etc/glexec/lcmaps" :
+      ensure => "directory",
+      owner => "root", group => "root", mode => 644,
+   }
+
+   file { "/etc/glexec/contrib/glexec_monitor" :
+   	ensure => "directory",
+		owner => "root", group => "root", mode => 644,	
+   }
+
+	file { "/etc/glexec/contrib/gums_interface" :
+      ensure => "directory",
+      owner => "root", group => "root", mode => 644,
+   }
+
+
+
+
 	# main glexec config
 	file { "/etc/glexec/testglexec":
-		ensure  => present,
+		ensure  => "present",
 		owner   => "root", group => "root", mode => 644,
 		source  => "puppet://red-man.unl.edu/wn-glexec/testglexec",
 	}
 
+	file { "/etc/glexec/glexec.conf":
+      ensure  => "present",
+      owner   => "root", group => "root", mode => 644,
+      source  => "puppet://red-man.unl.edu/wn-glexec/glexec.conf",
+   }
+	file { "/etc/glexec/tracking_groups.cfg":
+      ensure  => "present",
+      owner   => "root", group => "root", mode => 644,
+      source  => "puppet://red-man.unl.edu/wn-glexec/tracking_groups.cfg",
+   }
+   file { "/etc/glexec/contrib_monitor":
+      ensure  => "present",
+      owner   => "root", group => "root", mode => 644,
+      source  => "puppet://red-man.unl.edu/wn-glexec/contrib_monitor",
+   } 
+
+	file { "/etc/glexec/lcas/ban_users.db":
+      ensure  => "present",
+      owner   => "root", group => "root", mode => 644,
+      source  => "puppet://red-man.unl.edu/wn-glexec/lcas/ban_users.db",
+   }
+
+   file { "/etc/glexec/lcas/lcas-suexec.db":
+      ensure  => "present",
+      owner   => "root", group => "root", mode => 644,
+      source  => "puppet://red-man.unl.edu/wn-glexec/lcas/lcas-suexec.db",
+   }
+
+	file { "/etc/glexec/lcmaps/lcmaps-suexec.db":
+      ensure  => "present",
+      owner   => "root", group => "root", mode => 644,
+      source  => "puppet://red-man.unl.edu/wn-glexec/lcmaps/lcmaps-suexec.db",
+   }
+   
+	file { "/etc/glexec/contrib/glexec_monitor/glexec_monitor.cfg":
+      ensure  => "present",
+      owner   => "root", group => "root", mode => 644,
+      source  => "puppet://red-man.unl.edu/wn-glexec/contrib/glexec_monitor/glexec_monitor.cfg",
+   }
+   file { "/etc/glexec/contrib/gums_interface/getmapping.cfg":
+      ensure  => "present",
+      owner   => "root", group => "root", mode => 644,
+      source  => "puppet://red-man.unl.edu/wn-glexec/contrib/gums_interface/getmapping.cfg",
+   }
 }
 
