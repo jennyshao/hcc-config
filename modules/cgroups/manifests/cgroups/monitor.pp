8107a3d0eb97d44635141b08c4da6eed9e2a46c6
root <root@red-man.unl.edu>
2011-05-23 14:15:43 -0500

    changed my mind ... starting this module from scratch, not example42 base

--- modules/cgroups/manifests/cgroups/monitor.pp
+++
@@ -1,8 +0,0 @@
-# Class foo::cgroups::monitor
-#
-# Custom monitor class for cgroups project.
-# Here you can override or define your settings for the monitor wrapper
-#
-class foo::cgroups::monitor inherits foo::monitor {
-
-}
