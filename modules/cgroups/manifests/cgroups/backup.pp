9e3fdf48a1ef2f199b08e21f3b2ef9dfaa74e7fe
Garhan Attebury <attebury@cse.unl.edu>
2011-05-23 14:05:55 -0500

    initial cgroups module import from example42 foo base

---
+++ modules/cgroups/manifests/cgroups/backup.pp
@@ -0,0 +1,8 @@
+# Class foo::cgroups::backup
+#
+# Custom backup class for cgroups project.
+# Here you can override or define your settings for the backup wrapper
+#
+class foo::cgroups::backup inherits foo::backup {
+
+}
---
+++ modules/cgroups/manifests/cgroups/monitor.pp
@@ -0,0 +1,8 @@
+# Class foo::cgroups::monitor
+#
+# Custom monitor class for cgroups project.
+# Here you can override or define your settings for the monitor wrapper
+#
+class foo::cgroups::monitor inherits foo::monitor {
+
+}
