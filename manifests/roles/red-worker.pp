b61596cf20d1e3719592a890dbc82c7e48acbf5a
root <root@red-man.unl.edu>
2011-08-29 12:43:42 -0500

    added ganglia module to all workers overloaded appropriate vars to use red-workers ganglia cluster

--- manifests/roles/red-worker.pp
+++ manifests/roles/red-worker.pp
@@ -2,7 +2,13 @@ class role_red-worker {
 
 	$isCondorWorker = true
 
+	$gangliaClusterName = 'red-worker'
+	$gangliaTCPAcceptChannel = '8651'
+	$gangliaUDPSendChannel = [ 'red-mon.unl.edu', '8651' ]
+	$gangliaUDPRecvChannel = '8651'
+
 	include condor
+	include ganglia
 #	include cgroups
 	include fetch-crl
    include wn-glexec
