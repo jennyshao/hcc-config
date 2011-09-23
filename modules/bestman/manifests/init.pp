f486a8d53366e5a6c5d5a890a034d243b5b3fe6b
root <root@red-man.unl.edu>
2011-09-23 09:52:13 -0500

    bestman module tweaks (first really good version)

--- modules/bestman/manifests/init.pp
+++ modules/bestman/manifests/init.pp
@@ -2,9 +2,9 @@
 # Class: bestman
 #
 
-class xrootd {
+class bestman {
 
-	package { "bestman2":
+	package { "bestman2-server":
 		ensure => present,
 	}
 
@@ -14,18 +14,40 @@ class xrootd {
 		enable => true,
 		hasrestart => true,
 		hasstatus => true,
-		require => Package["bestman2"],
-		subscribe => File["bestman2.rc"],
+		require => [ Package["bestman2-server"], File["sudo-bestman"], File["bestman-httpcert.pem"], File["bestman-httpkey.pem"], ],
+		subscribe => [ File["bestman2.rc"], File["bestman-httpcert.pem"], File["bestman-httpkey.pem"], ],
 	}
 
 	file { "bestman2.rc":
-		path    => "/opt/bestman2/conf/bestman2.rc",
+		path    => "/etc/bestman2/conf/bestman2.rc",
 		owner   => "root", group => "root", mode => 644,
-		require => Package["bestman2"],
+		require => Package["bestman2-server"],
 		content => template("bestman/bestman2.rc.erb"),
 	}
 
- ### certs
+
+	file { "sudo-bestman":
+		path    => "/etc/sudoers.d/sudo-bestman",
+		owner   => "root", group => "root", mode => 0440,
+		require => Class["sudo"],
+		source  => "puppet:///modules/bestman/sudo-bestman",
+	}
+
+
+	# custom http certs for betman
+	file { "bestman-httpcert.pem":
+		path    => "/etc/grid-security/http/bestman-httpcert.pem",
+		owner   => "bestman", group => "bestman", mode => 644,
+		require => File["/etc/grid-security/http/httpcert.pem"],
+		source  => "/etc/grid-security/http/httpcert.pem",
+	}
+
+	file { "bestman-httpkey.pem":
+		path    => "/etc/grid-security/http/bestman-httpkey.pem",
+		owner   => "bestman", group => "bestman", mode => 600,
+		require => File["/etc/grid-security/http/httpkey.pem"],
+		source  => "/etc/grid-security/http/httpkey.pem",
+	}
 
 }
 
