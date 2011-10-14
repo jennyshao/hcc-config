addd44eb2f0c23c2efa8f7f32cfdfe6a1cfdf04c
root <root@red-man.unl.edu>
2011-10-13 21:36:05 -0500

    check for new osg-wn-client-glexec install and use it if present while our CEs still point to some /opt/osg/... nonsense
    back to 4 cores on THPC nodes with no CMS jobs

--- modules/condor/files/condor_nfslite_job_wrapper.sh
+++ modules/condor/files/condor_nfslite_job_wrapper.sh
@@ -9,6 +9,12 @@
 ##############################################################################
 
 
+# fix for new osg-wn-client-glexec RPM installs, CEs still point at old location
+if [ -f /usr/sbin/glexec ]
+   then export OSG_GLEXEC_LOCATION="/usr/sbin/glexec"
+fi
+
+
 if [ -n "$MY_INITIAL_DIR" ]
 then
     eval cd $MY_INITIAL_DIR
