52e3ab6abc083c5813911fd9369df63e553bcd63
root <root@red-man.unl.edu>
2011-10-26 13:32:40 -0500

    some additions to our gatekeepers ... getting closer to being normal

--- manifests/roles/red-gatekeeper.pp
+++ manifests/roles/red-gatekeeper.pp
@@ -1,8 +1,14 @@
 class role_red-gatekeeper {
 
+   $pakitiTag = "T2_US_Nebraska"
+
 	include condor
 	include fetch-crl
+	include pakiti
 	include ganglia::diskstat
 	include osg-ce
+
+	include sudo
+	include nrpe
 
 }
