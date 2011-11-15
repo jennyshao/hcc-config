d131cfa03b8a1aba661e24aa2281102cfa4b4b79
Garhan Attebury <attebury@cse.unl.edu>
2011-11-15 07:30:49 -0600

    almost complete rework of module to use param, install, config, and service classes

--- modules/ganglia/manifests/init.pp
+++ modules/ganglia/manifests/init.pp
@@ -5,51 +5,6 @@
 #
 class ganglia {
 
-	package { ganglia-gmond:
-		name   => "ganglia-gmond", 
-		ensure => latest,
-	}
-
-	package { ganglia-gmond-modules-python:
-		name   => "ganglia-gmond-modules-python",
-		ensure => latest,
-	}
-
-
-	group { "ganglia":
-		name => "ganglia",
-		ensure => present,
-	}
-
-	user { "ganglia":
-		name => "ganglia",
-		ensure => present,
-		system => true,
-		gid => "ganglia",
-		managehome => false,
-		shell => "/sbin/nologin",
-	}
-
-
-	service { "gmond":
-		name       => "gmond",
-		ensure     => running,
-		enable     => true,
-		hasrestart => true,
-		hasstatus  => true,
-		require    => [ Package["ganglia-gmond"], User["ganglia"], ],
-		subscribe  => File["gmond.conf"],
-	}
-
-
-	file { "gmond.conf":
-		path    => "/etc/ganglia/gmond.conf",
-		mode    => 644,
-		owner   => "root",
-		group   => "root",
-		content => template("ganglia/gmond.conf.erb"),
-		require => [ Package[ganglia-gmond], User["ganglia"], ],
-		ensure  => present,
-	}
+	include ganglia::params, ganglia::install, ganglia::config, ganglia::service
 
 }
