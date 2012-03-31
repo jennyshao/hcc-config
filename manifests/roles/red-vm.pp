6a698338a76606be4d84b831c2c5e0ff7e0971bd
Garhan Attebury <attebury@cse.unl.edu>
2012-03-31 17:28:52 -0500

    removed manual yum repo simplified to just red-vm module for now

--- manifests/roles/red-vm.pp
+++ manifests/roles/red-vm.pp
@@ -1,17 +1,5 @@
 class role_red-vm {
 
-#	include ganglia
-#	include openldap
-
 	include red-vm
-
-	yumrepo { "nebraska-el6":
-		baseurl => "http://t2.unl.edu/store/repos/nebraska/6/nebraska-el6/x86_64/",
-		descr => "Nebraska EL6 Packages",
-		enabled => 1,
-		gpgcheck => 0,
-		http_caching => none,
-		priority => "97",
-	}
 
 }
