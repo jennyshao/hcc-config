513bff9bbfb55826ce127b5ba02bfbd3da2ca9a2
Garhan Attebury <attebury@cse.unl.edu>
2012-03-31 17:37:30 -0500

    ugg ... stuff

--- manifests/roles/red-worker-el6.pp
+++ manifests/roles/red-worker-el6.pp
@@ -2,6 +2,8 @@ class role_red-worker-el6 {
 
 	$isCondorWorker = true
 
+   $mountsHDFS = true
+
 	$gangliaClusterName = 'red-worker'
 	$gangliaTCPAcceptChannel = '8651'
 	$gangliaUDPSendChannel = [ 'red-mon.unl.edu', '8651' ]
@@ -13,11 +15,39 @@ class role_red-worker-el6 {
    include osg-wn-client
    include glexec
    include cvmfs
+	include chroot
    include osg-ca-certs
    include hadoop
+   include cgroups
+   include nrpe
+   include sudo
 
-## These are not yet working
-#  include pakiti
-#	include cgroups
+
+   ## TODO: This is copy/paste from red-worker57; unify.
+
+   # must hard mount OSGAPP and OSGDATA to make RSV probes happy
+   # automounting will not show correct permissions
+   file { "/opt/osg": ensure => directory }
+   file { "/opt/osg/app": ensure => directory }
+   mount { "mount_osg_app":
+      name    => "/opt/osg/app",
+      device  => "hcc-gridnfs:/osg/app",
+      fstype  => "nfs4",
+      ensure  => mounted,
+      options => "rw,noatime,hard,intr,rsize=32768,wsize=32768",
+      atboot  => true,
+      require => [ File["/opt/osg"], File["/opt/osg/app"], ],
+   }
+   
+   file { "/opt/osg/data": ensure => directory }
+   mount { "mount_osg_data":
+      name    => "/opt/osg/data",
+      device  => "hcc-gridnfs:/osg/data",
+      fstype  => "nfs4",
+      ensure  => mounted,
+      options => "rw,noatime,hard,intr,rsize=32768,wsize=32768",
+      atboot  => true,
+      require => [ File["/opt/osg"], File["/opt/osg/data"], ],
+   }
 
 }
