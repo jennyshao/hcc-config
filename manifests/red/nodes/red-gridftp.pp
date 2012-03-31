df7307c47c91cdefcaaf35055a9a4e3a0a63d199
Garhan Attebury <attebury@cse.unl.edu>
2012-03-31 17:30:20 -0500

    removed red-gridftp11 specializations (don't ever do this again)
    removed dups from baselines/general class
    
    ... manually setting extra yum repos, not really needed

--- manifests/red/nodes/red-gridftp.pp
+++ manifests/red/nodes/red-gridftp.pp
@@ -1,31 +1,10 @@
 ### red gridftp / xrootd servers
 
-node 'red-gridftp11.unl.edu' inherits red-public {
-	include general
-
-#	$yum_extrarepo = [ 'nebraska', 'osg' ]
-#	include yum
-
-	include ganglia
-	include gridftp-hdfs
-}
-
-
 node /^red-gridftp\d+\.unl\.edu$/ inherits red-public {
 
-#	$role = "red-gridftp"
-
+	$yum_extrarepo = [ 'epel', 'nebraska', 'osg' ]
+	$role = "red-gridftp"
 	include general
-	include users
-	include autofs
-	include pam
-	include openssh
-	include sudo
-	include nrpe
-	include ganglia
-	include hostcert
-	include gridftp-hdfs
-	include xrootd
 
 }
 
