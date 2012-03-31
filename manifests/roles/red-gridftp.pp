be00259e52348e977cb4546c0f3c8d7fdc664349
Garhan Attebury <attebury@cse.unl.edu>
2012-03-31 17:29:26 -0500

    removed duplicates from baselines/general class

--- manifests/roles/red-gridftp.pp
+++ manifests/roles/red-gridftp.pp
@@ -1,15 +1,9 @@
+#
+# red gridftp/xrootd
+#
+
 class role_red-gridftp {
 
-   include general
-   include users
-   include autofs
-   include pam
-   include openssh
-   include sudo
-   include nrpe
-   include ganglia
-   include ganglia::diskstat
-   include hostcert
    include gridftp-hdfs
    include xrootd
 
