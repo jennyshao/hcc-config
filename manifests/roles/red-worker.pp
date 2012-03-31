0b9e516f9fcd9ebb6f8651e1ba51ef4f447f5de8
Garhan Attebury <attebury@cse.unl.edu>
2012-03-31 17:32:24 -0500

    manually setting all extra yum repos (not needed)
    someone commented out wn-glexec, why?

--- manifests/roles/red-worker.pp
+++ manifests/roles/red-worker.pp
@@ -6,11 +6,12 @@ class role_red-worker {
 	$gangliaTCPAcceptChannel = '8651'
 	$gangliaUDPSendChannel = [ 'red-mon.unl.edu', '8651' ]
 	$gangliaUDPRecvChannel = '8651'
+	$yum_extrarepo = [ 'epel', 'nebraska', 'osg' ]
 
 	include condor
 	include ganglia
 #	include cgroups
 	include fetch-crl
-   include wn-glexec
+#  include wn-glexec
 
 }
