b792ddad24bcc57432c41cc42d50c47f44f50afe
Garhan Attebury <attebury@cse.unl.edu>
2012-03-31 14:01:34 -0500

    some tweaks while testing inline templating to allow custom tempaltes per hostname

--- modules/tester/manifests/init.pp
+++ modules/tester/manifests/init.pp
@@ -4,19 +4,13 @@
 
 class tester {
 
-	group { "xrootd":
-		name => "xrootd",
-		ensure => present,
-		gid => 108,
-	}
+	file { "puppet-tester":
 
-	user { "xrootd":
-		name => "xrootd",
-		ensure => present,
-		uid => 108,
-		gid => "xrootd",
-		managehome => false,
-		shell => "/sbin/nologin",
+		path => "/tmp/puppet-tester",
+		content => $lsbmajdistrelease ? {
+			6 => inline_template(file("/etc/puppet/modules/tester/templates/tester-$hostname.erb", "/etc/puppet/modules/tester/templates/tester.erb")),
+			default => "fiveish",
+		},
+		owner => root, group => 0, mode => 0644 ;
 	}
-
 }
