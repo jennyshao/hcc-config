ece5cfddf16a6c710a0325fc164262363a6fa51f
Garhan Attebury <attebury@cse.unl.edu>
2012-04-03 10:56:21 -0500

    removed idmapd management from nfs module, as it's taken care of in it's own module now and should be included on everything

--- modules/nfs/manifests/init.pp
+++ modules/nfs/manifests/init.pp
@@ -8,27 +8,4 @@ class nfs {
 
 	package { "nfs-utils": ensure => present, }
 
-	# idmapd needed for nfs4 with same domains (hcc.unl.edu in our case)
-
-	service { "rpcidmapd":
-		name => "rpcidmapd",
-		ensure => running,
-		enable => true,
-		hasrestart => true,
-		hasstatus => true,
-		require => Package["nfs-utils"],
-		subscribe => File["idmapd.conf"],
-	}
-
-	file { "idmapd.conf":
-		path => "/etc/idmapd.conf",
-		owner => "root", group => "root", mode => "0640",
-		require => Package["nfs-utils"],
-		notify => Service["rpcidmapd"],
-		source => $lsbmajdistrelease ? {
-			6 => "puppet:///nfs/idmapd.conf-rhel6",
-			default => "puppet:///nfs/idmapd.conf-rhel5",
-		}
-	}
-
 }
