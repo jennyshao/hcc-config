da16e3e08d744fcc7869b26aa1d5d87d890ce127
Garhan Attebury <attebury@cse.unl.edu>
2012-07-19 09:28:46 -0500

    some host-specific config changes and additions
    merged multipath files into a single instance

--- modules/red-vm/manifests/init.pp
+++ modules/red-vm/manifests/init.pp
@@ -3,24 +3,29 @@
 #
 class red-vm {
 
-#	package {
-#		"cman":	ensure => present,
-#		"iscsi-initiator-utils":	ensure => present,
-#	}
+	package { [ "cman", "libvirt", "qemu-kvm", "qemu-kvm-tools", "lvm2-cluster", "python-virtinst", "virt-viewer", "virt-manager", "iscsi-initiator-utils", "syslinux", ]:
+		ensure => present,
+	}
 
+	file { "iscsid.conf":
+		ensure => present,
+		path => "/etc/iscsi/iscsid.conf",
+		mode => "0644", owner => "root", group => "root",
+		source => [ "puppet:///modules/red-vm/iscsid.conf-$hostname", "puppet:///modules/red-vm/iscsid.conf", ],
+	}
 
 	file { "multipath.conf":
 		ensure => present,
 		path => "/etc/multipath.conf",
 		mode => "0644", owner => "root", group => "root",
-		source => "puppet:///modules/red-vm/multipath.conf-$hostname",
+		source => [ "puppet:///modules/red-vm/multipath.conf-$hostname", "puppet:///modules/red-vm/multipath.conf", ],
 	}
 
 	file { "lvm.conf":
 		ensure => present,
 		path => "/etc/lvm/lvm.conf",
 		mode => "0644", owner => "root", group => "root",
-		source => "puppet:///modules/red-vm/lvm.conf-$hostname",
+		source => [ "puppet:///modules/red-vm/lvm.conf-$hostname", "puppet:///modules/red-vm/lvm.conf", ],
 	}
 
 	service { "multipathd":
@@ -31,5 +36,7 @@ class red-vm {
 		require => File["multipath.conf"],
 		subscribe => File["multipath.conf"],
 	}
+
+	# no iscsid here, better to do that manually
 
 }
