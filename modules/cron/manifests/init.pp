5cbc6f14d60a296c9ccdeb152771ad9401c4929d
Garhan Attebury <attebury@cse.unl.edu>
2012-10-18 10:58:56 -0500

    allow custom cron templates per host

--- modules/cron/manifests/init.pp
+++ modules/cron/manifests/init.pp
@@ -7,7 +7,7 @@ class cron {
 	file { "cron.allow":
 		path    => "/etc/cron.allow",
 		owner   => "root", group => "root", mode => 644,
-		content => template("cron/cron.allow.erb"),
+		content => inline_template(file("/etc/puppet/modules/cron/templates/cron.allow-$hostname.erb", "/etc/puppet/modules/cron/templates/cron.allow.erb")),
 	}
 
 }
