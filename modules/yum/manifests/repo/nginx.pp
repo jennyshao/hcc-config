e511ee7b7748558b22266dedc2caa9ba2cff0f31
Garhan Attebury <attebury@cse.unl.edu>
2012-11-07 10:16:15 -0600

    fixed escaping issue on nginx repo ... at least I think that's what it does

--- modules/yum/manifests/repo/nginx.pp
+++ modules/yum/manifests/repo/nginx.pp
@@ -4,8 +4,8 @@ class yum::repo::nginx {
 	yum::managed_yumrepo {
 
 	'nginx':
-		descr => "nginx repo for RHEL\$lsbmajdistrelease",
-		baseurl => "http://nginx.org/packages/rhel/\$lsbmajdistrelease/\$basearch/",
+		descr => "nginx repo for RHEL$lsbmajdistrelease",
+		baseurl => "http://nginx.org/packages/rhel/$lsbmajdistrelease/\$basearch/",
 		enabled => 1,
 	 	gpgcheck => 0,
 		priority => 8 ;
