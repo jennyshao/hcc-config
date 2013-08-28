38ff26b97e02349e83ef728458ef7cc0fc175193
John Thiltges <jthiltges2@unl.edu>
2013-08-28 11:27:20 -0500

    Renamed existing chroot module to chroot5 (RHEL5) and added new chroot module

--- modules/chroot/manifests/params.pp
+++ modules/chroot/manifests/params.pp
@@ -1,13 +1,3 @@
 class chroot::params {
-
-   $chroot_top  = "/chroot"
-
-   $chroot_version = "sl5-v5"
-
-   $chroot_base = "${chroot_top}/${chroot_version}"
-
-	$chroot_root = "${chroot_base}/root"
-
-   $chroot_user = "chroot_creator"
-
+  $base_dir = '/chroot'
 }
